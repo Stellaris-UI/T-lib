2a8851ec800f208266311d53076c473640b708ff
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-12 21:34:16 -0600

    Re-btn mockup

---
+++ src/re-test/re-testComponent.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { Retestprops } from "./re-testComponent.types";
+
+const ReTestComponent: React.FC<Retestprops> = ({ theming }) => (
+  <Fragment>
+    <button data-testid="ReTestButton" className={`re-btn re-btn-${theming}`}>
+      Hi, from the re-test btn component
+    </button>
+  </Fragment>
+);
+
+export default ReTestComponent;
---
+++ src/re-test/re-testComponent.types.ts
@@ -0,0 +1,3 @@
+export interface Retestprops {
+  theming: "light" | "dark";
+}
