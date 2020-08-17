8758ba86a487674e8f855525db3300b50fc6ea60
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-16 23:21:50 -0600

    Added some Rollup changes and the example container component was added

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,7 @@
+// Read:
+// Why code splitting components? On
+// https://github.com/Stellaris-UI/t-lib/blob/master/README.md
+
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
@@ -7,19 +11,29 @@ import postcss from "rollup-plugin-postcss";
 const packageJson = require("./package.json");
 
 export default {
-  input: "src/index.ts",
+  input: [
+    "src/index.ts",
+    "src/container-test/container-testComponent.tsx",
+    "src/re-test/re-testComponent.tsx"
+  ],
+  // Also de input is way diferent when using code splitting
+  // we use the direct component to bundle per component
+  // input: "src/index.ts",
   output: [
     {
       file: packageJson.main,
       format: "cjs",
       sourcemap: true
     },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
+    //This is in case we don't need code splitting between components
+    // {
+    //   file: packageJson.module,
+    //   format: "esm",
+    //   sourcemap: true
+    // }
   ],
+  //Added preserveModules for code splitting
+  preserveModules:true,
   plugins: [
     peerDepsExternal(),
     resolve(),
